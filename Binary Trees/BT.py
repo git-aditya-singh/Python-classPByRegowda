5beed06359e23aa4f7bd603441c8f5ad0ec86b6a
aditya <mail2adityasinghsln@gmail.com>
2023-10-24 20:34:55 +0530

    Binary Tree

--- Binary Trees/BT.py
+++ Binary Trees/BT.py
@@ -27,7 +27,7 @@ def construct(arr):
                 st.append(p1)
             top.vis+=1
             idx+=1
-        elif(top.state==2):
+        elif(top.vis==2):
             if(arr[idx]!=None):
                 rc=Node(arr[idx])
                 top.node.right=rc
@@ -38,3 +38,31 @@ def construct(arr):
         else:
             st.pop()
     return root
+
+def constructRec(arr,currIdx):
+    if(currIdx==len(arr) or arr[currIdx]==None):
+        return None,currIdx+1
+
+    root=Node(arr[currIdx])
+    #faith
+    lc,index1=constructRec(arr,currIdx+1)
+    rc,index2=constructRec(arr,index1)
+    root.left=lc
+    root.right=rc
+    return root,index2
+
+def display(root):
+    if(root==None):
+        return
+
+    left= "." if root.left==None else str(root.left.data)
+    right= "." if root.right==None else str(root.right.data)
+    ans=left+"<-"+str(root.data)+"->"+right
+    print(ans)
+    #faith
+    display(root.left)
+    display(root.right)
+
+arr=[50,25,32,None,None,23,19,None,None,None,75,41,17,None,None,None,68,None,None]
+root,ind=constructRec(arr,0)
+display(root)
